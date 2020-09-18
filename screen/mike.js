8caf487f1ad96b3d92190e09c20da3efe379a77f
Michael Kresslein 1 <michael.kresslein@maxar.com>
2020-09-18 09:13:15 -0400

    remove screen

--- screen/mike.js
+++
@@ -1,7 +0,0 @@
-var text = '{ "screen" : [' +
-	'{
-		"name" : "Michael Kresslein",
-		"favorite" : "history",
-		"code" : "NzY1Y2Y4ZGYwMWQ1ZTgyNmE1NTRmNWJiNjNhMDc5OWY4ZjExYWJmMD1leHBpcmVzLTE1NTUxNzM3MDYzOTM7RXhwaXJlcz1TYXQsIDEzIEFwciAyMDE5IDE2OjQxOjQ2IEdNVDtMYW5nPUVO"
-	}
-]}';
